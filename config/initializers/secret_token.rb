a7f569176a98073c402dde05f1ff2a32455aab1a
yalab <rudeboyjet@gmail.com>
2013-07-27 11:47:46 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nezumi::Application.config.secret_key_base = 'a8bd65ebc33ae84aec72eac4820f70e54445ac240bf3ba7aee62d6ea9170d82de0204a47307468b89802c5df828351f8493a98684f749d4787e4f7a087db5569'
